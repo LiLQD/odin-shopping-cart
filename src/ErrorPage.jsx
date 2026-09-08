d1fa0c6750fa8389d72a816e0b6ff3f4df9d480c
LiLQD <luuquangdung1511@gmail.com>
2026-09-08 21:05:15 +0700

    feat: add ErrorPage.jsx

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,2 +1,16 @@
-const ErrorPage = () => {};
+import { Link } from "react-router";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <div>✦ ᛉ ✦ ᛟ ✦ ᚠ ✦</div>
+      <div>404</div>
+      <div>🐉</div>
+      <h1>The Realm is Lost</h1>
+      <Link to="/">Return to the Gate</Link>
+      <div>✦ ᚠ ✦ ᛟ ✦ ᛉ ✦</div>
+    </div>
+  );
+};
+
 export default ErrorPage;
