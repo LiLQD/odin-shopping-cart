8d239ed3df86cfebc71a5856a2b81fde8ccdafcb
LiLQD <luuquangdung1511@gmail.com>
2026-09-11 14:36:25 +0700

    feat: add reset css styling file

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import "./styles/Reset.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
